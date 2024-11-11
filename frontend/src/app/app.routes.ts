7fc33cb6ea4a30b372fb83f70a56cf0f26d68545
Dobai Lorena <lorena_dobai@yahoo.com>
2024-11-11 21:18:43 +0200

    did the help page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component'; 
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { HelpComponent } from './components/help/help.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent }, 
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'help', component: HelpComponent } 
 ];
 
 @NgModule({
