a58f9beaabb7bcdfb475f33165d6905ce82c876b
Dobai Lorena <lorena_dobai@yahoo.com>
2024-11-18 22:02:49 +0200

    did capacitor on angular in order to start android app

---
+++ frontend/android/app/src/main/java/com/exemplu/swapbook/MainActivity.java
@@ -0,0 +1,5 @@
+package com.exemplu.swapbook;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.exemplu.swapbook',
+  appName: 'BookSwap',
+  webDir: 'dist/BookSwap/browser'
+};
+
+export default config;
